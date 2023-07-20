32a3956f81cc587f8227190b128a0f7560f30f31
sathishKumarMernDev <sathish.kumar@10decoders.in>
2023-07-20 17:15:37 +0530

    not found image min and max height added

--- src/Pages/404/index.tsx
+++ src/Pages/404/index.tsx
@@ -7,11 +7,13 @@ const NotFoundPage = () => {
       <img
         src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7898.jpg?w=2000"
         width="100%"
-        height="500px"
+        height="100%"
         alt="404"
         style={{
           marginTop: '40px',
-          borderRadius: '10px'
+          borderRadius: '10px',
+          minHeight: '250px',
+          maxHeight: '500px'
         }}
       />
     </Container>
