7c576f94d34673e90c94fa28f3c40d3b951976ed
sathishKumarMernDev <sathish.kumar@10decoders.in>
2023-07-20 16:48:48 +0530

    not found image height updated

--- src/Pages/404/index.tsx
+++ src/Pages/404/index.tsx
@@ -7,7 +7,12 @@ const NotFoundPage = () => {
       <img
         src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7898.jpg?w=2000"
         width="100%"
+        height="500px"
         alt="404"
+        style={{
+          marginTop: '40px',
+          borderRadius: '10px'
+        }}
       />
     </Container>
   );
